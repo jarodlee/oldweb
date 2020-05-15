c071f8b9daf585cc67b0d5bbe91bf548975259b3
anqing.de <lee.jarod@gmail.com>
2020-05-15 20:20:15 +0800

    恢复旧网站的代码
    主要把HTML文件恢复到github

---
+++ zf/index.php
@@ -0,0 +1,124 @@
+
+<html>
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=gb2312">
+
+<meta name="GENERATOR" content="Microsoft FrontPage 4.0">
+
+<style type="text/css">
+<!--
+
+td,body {  font-size: 9pt}
+
+a:link{ color:#000000; text-decoration:none}     
+
+a:visited{COLOR: #000000; TEXT-DECORATION: none} 
+
+a:active{color:green;text-decoration:none}
+
+a:hover{color:red;text-decoration:underline} .style4 {font-size:12px;font-family:宋体; line-height: 18px}
+.style5 {font-size:12px;font-family:宋体; line-height: 18px;color:green;}
+.style14 {
+	font-size:14px;
+	font-family:宋体;
+	font-weight: bold;
+	line-height: 20px;
+}
+.style20 {
+	font-size:20px;
+	font-family:宋体;
+	font-weight: bold;
+	line-height: 25px;
+	color:green;
+}
+body {
+	margin-top: 0px;
+	background-image: url(images/index_32.gif);
+}
+.STYLE24 {font-family: "新宋体"; line-height: 20px; color: #000000; font-size: medium;}
+.STYLE25 {color: #FFFFFF}
+--> 
+
+</style>
+<SCRIPT src="images/js.js"></SCRIPT>
+
+<base target="_blank">
+
+<title>干海棠学习中心祝您圣诞快乐，心想事成，万事如意-----干海棠圣诞节祝福网页</title>
+
+</head>
+
+
+
+<body>
+<table width="760"  border="0" align="center" cellpadding="3" cellspacing="1" bgcolor="CAE6BF">
+  <tr>
+    <td bgcolor=E2F7DA><table width="100%"  border="0" cellpadding="0" cellspacing="0" bgcolor="#FFFFFF">
+      <tr>
+        <td align="center"><table width="100%"  border="0" cellspacing="0" cellpadding="2">
+          <tr>
+            <td width="99%" background="images/1.gif" class="style20"><span class="STYLE24">请在下面输入你的名字</span>
+              <FORM name=newinfo_form onSubmit="return check(this)">
+                      <INPUT id=stra  tabIndex=1 maxLength=8 size=16 name=stra>
+                      <INPUT type=submit value=点此提交 name=Submit>
+                  </FORM></td>
+          </tr>
+          <tr >
+            <td height="1" colspan="2" background="../hudonghd/images/dot_05.gif"></td>
+          </tr>
+          <tr>
+            <td colspan="2" align="center" bgcolor="#663399" class="f9"><p align="center" class="style20"><span class="STYLE25">又是一年的“圣诞节”，</span><br>
+                        <font color="#F84471">
+                          <SCRIPT>document.write(lusername)</SCRIPT>
+                    </font><span class="STYLE25">通过</span> <a href="http://www.ghtxx.cn" target="_blank"><font color="green">干海棠</a> </a><span class="STYLE25">想对你说：</span></p></td>
+          </tr>
+          <tr >
+            <td height="1" colspan="2" background="../hudonghd/images/dot_05.gif"></td>
+          </tr>
+          <tr>
+            <td  height="1" colspan="2" background="../hudonghd/images/dot_05.gif"></td>
+          </tr>
+          <tr>
+            <td  height="1" colspan="2" background="../hudonghd/images/dot_05.gif"></td>
+          </tr>
+          <tr>
+            <td  height="1" colspan="2" background="../hudonghd/images/dot_05.gif"></td>
+          </tr>
+          <tr>
+            <td colspan="2" align="center" class="f9"><IFRAME border=0 frameBorder=no height="430" marginHeight=0 marginWidth=0 scrolling=yes src="index_zhufu.html" width="100%"></IFRAME></td>
+          </tr>
+          <tr>
+            <td  height="1" colspan="2" background="../hudonghd/images/dot_05.gif"></td>
+          </tr>
+          <tr>
+            <td height="60" colspan="2" valign="top" bgcolor="#663399" class="style14">　　<span class="STYLE25">如果你想送朋友祝福，请在下面输入你的名字，将会自动生成含有你名字的个性祝福，在圣诞节前夕，给你的朋友送去一份意外的惊喜吧！！</span></td>
+          </tr>
+          <tr>
+            <td colspan="2" align="center" bgcolor="#663399" class="style14"><span class="STYLE25">请在下面输入你的名字</span><br>
+                    <FORM name=newinfo_form onSubmit="return check(this)">
+                      <INPUT id=stra  tabIndex=1 maxLength=8 size=16 name=stra2>
+                      <INPUT type=submit value=点此提交 name=Submit>
+                </FORM></td>
+          </tr>
+          
+          
+          
+        </table></td>
+      </tr>
+    </table></td>
+  </tr>
+</table>
+<!-- google-analytics -->
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-482604-4");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+<!-- /google-analytics --> 
+</body>
+</html>
