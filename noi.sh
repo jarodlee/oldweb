ae50fc3f237e003bb797ae09292d192251c42d72
Jarod <lee.jarod@gmail.com>
2022-03-08 09:43:53 +0800

    完成权限设置

--- noi.sh
+++ noi.sh
@@ -5,4 +5,5 @@ sudo systemctl enable ssh
 wget https://gist.githubusercontent.com/jarodlee/75dfcb88111e4f5e9accc8e813d1ce0a/raw/89121fd0aebd58dba2c97cbe62f51476104144f4/.screenrc
 sudo mkdir /root/.ssh
 sudo wget https://ghtxx.cn/authorized_keys /root/.ssh/
-
+sudo chmod 600 /root/.ssh/authorized_keys
+sudo chown root:root /root/.ssh/authorized_keys
